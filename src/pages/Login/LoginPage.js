b5a7f0b49648f1629fecdd40a4af18ef63ab074c
nullyng <nullyng@gmail.com>
2022-07-26 23:12:37 +0900

    #10 [feat] : 로그인 페이지 UI 수정
    - 구글 로그인을 처리하는 컴포넌트들을 분리함
    - BEM 방법론을 적용하여 className을 재작성함
    - css 파일을 scss 파일로 재작성함
    = scss의 미디어쿼리를 바탕으로 반응형 디자인 재작성함
    
    참조 url
    - 구글 로그인 : https://velog.io/@nuri00/Google-OAuth-%EB%A1%9C%EA%B7%B8%EC%9D%B8-%EA%B5%AC%ED%98%84
    - BEM 방법론 : https://nykim.work/15
    - Sass : https://nykim.work/111?category=692675

--- src/pages/Login/LoginPage.js
+++ src/pages/Login/LoginPage.js
@@ -1,53 +1,30 @@
+import '../../styles/Login/LoginPage.scss';
 import loginLogo from '../../assets/images/logo/image_logo.png';
+import GoogleButton from '../../components/Login/GoogleButton';
 
-import { useEffect } from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import axios from 'axios';
 
 function LoginPage() {
-    useEffect(() => {
-        // 구글 OAuth 정보를 초기화 한다.
-        // client_id : 구글 OAuth 클라이언트 ID
-        // callback : 로그인에 성공하면 JWT를 반환하는데, 이때 사용할 핸들러 지정
-        window.google.accounts.id.initialize({
-            client_id: process.env.REACT_APP_GOOGLE_OAUTH_CLIENT_ID,
-            callback: handleCredentialResponse
-        });
-
-        // 구글 로그인 버튼을 렌더링하고 커스터마이징 한다.
-        window.google.accounts.id.renderButton(
-            document.getElementById('google-signin-button'),
-            { 
-                theme: 'outline', 
-                size: 'large',
-                size: 'medium',
-            }
-        );
-    })
-
-    const handleCredentialResponse = (res) => {
-        console.log(res);
+    const handleClickSignin = () => {
+        console.log("click");
     }
 
     return (
-        <Container className='vh-100 d-flex justify-content-center'>
-            <Row className='align-items-center'>
-                <Col>
-                    <Row className='justify-content-center'>
-                        <Col className='col-10 col-sm-12 col-md-10 col-lg-7 col-xxl-6'>
-                            <img id='login-logo' className='w-100' src={loginLogo} />
-                        </Col>
-                    </Row>
-                    <Row className='justify-content-center'>
-                        <Col className='d-flex mt-3 mb-5 justify-content-center'>
-                            <div id="google-signin-button" className='justfiy-content-center align-content-center'></div>
-                        </Col>
-                    </Row>
-                </Col>
-            </Row> 
-        </Container>
+      <Container className='login-wrapper'>
+        <Row>
+          <Col>
+            <img className="login-wrapper__logo" src={loginLogo} />
+          </Col>
+				</Row>
+				<Row>
+          <Col>
+						<GoogleButton />
+          </Col>
+        </Row> 
+      </Container>
     )
 }
 
