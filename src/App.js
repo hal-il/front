9d1267682d357d416ef891a51e3c3205fe7c4e7d
nullyng <nullyng@gmail.com>
2022-07-26 23:04:03 +0900

    #10 [feat] : 로그인 페이지 라우터 등록
    - 로그인 페이지에 대한 라우터 등록
    - 구글 로그인 후 리다이렉트에 대한 라우터 등록

--- src/App.js
+++ src/App.js
@@ -2,11 +2,18 @@ import logo from './logo.svg';
 import './App.css';
 
 import LoginPage from './pages/Login/LoginPage';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import GoogleRedirectHandler from 'components/Login/GoogleRedirectHandler';
 
 function App() {
   return (
     <div className="App">
-      <LoginPage />
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<LoginPage />} />
+          <Route path="/oauth/callback" element={<GoogleRedirectHandler />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
