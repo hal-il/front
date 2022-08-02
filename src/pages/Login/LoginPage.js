bfd26c7b8b826d0cb38c59274c17142cc2a38eee
nullyng <nullyng@gmail.com>
2022-08-02 21:30:47 +0900

    #10 [chore] : 절대경로 적용 및 사용하지 않는 모듈 삭제

--- src/pages/Login/LoginPage.js
+++ src/pages/Login/LoginPage.js
@@ -1,27 +1,26 @@
-import '../../styles/Login/LoginPage.scss';
-import loginLogo from '../../assets/images/logo/image_logo.png';
-import GoogleButton from '../../components/Login/GoogleButton';
+import "styles/Login/LoginPage.scss";
+import loginLogo from "assets/images/logo/image_logo.png";
+import GoogleButton from "components/Login/GoogleButton";
 
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import axios from 'axios';
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 function LoginPage() {
-    return (
-      <Container className='login-wrapper'>
-        <Row>
-          <Col>
-            <img className="login-wrapper__logo" src={loginLogo} />
-          </Col>
-				</Row>
-				<Row>
-          <Col>
-						<GoogleButton />
-          </Col>
-        </Row> 
-      </Container>
-    )
+  return (
+    <Container className="login-wrapper">
+      <Row>
+        <Col>
+          <img className="login-wrapper__logo" src={loginLogo} />
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <GoogleButton />
+        </Col>
+      </Row>
+    </Container>
+  );
 }
 
 export default LoginPage;
