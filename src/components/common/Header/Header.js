69b82a085b4b0cae9192a187fc0e17cdc41b177f
nullyng <nullyng@gmail.com>
2022-09-04 18:42:40 +0900

    #13 [feat] : 헤더 UI 생성
    - MUI의 Appbar를 사용해 헤더 UI 생성
    - styled-component를 사용해 스타일 적용
    
    참조 url : https://velog.io/@minju1009/Styled-Components-with-MUI

--- src/components/common/Header/Header.js
+++ src/components/common/Header/Header.js
@@ -1,3 +1,49 @@
-function Header() {}
+import styled from "@emotion/styled";
+import {
+  AppBar,
+  IconButton,
+  StyledEngineProvider,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import PeopleAltOutlinedIcon from "@mui/icons-material/PeopleAltOutlined";
+import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
+import MenuOutlinedIcon from "@mui/icons-material/MenuOutlined";
+import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined";
+
+function Header() {
+  return (
+    <StyledEngineProvider injectFirst>
+      <AppBar position="static">
+        <MyToolbar>
+          <Typography variant="h8" component="div" sx={{ flexGrow: 1 }}>
+            &nbsp;
+          </Typography>
+          <MyIconButton>
+            <PeopleAltOutlinedIcon />
+          </MyIconButton>
+          <MyIconButton>
+            <SearchOutlinedIcon />
+          </MyIconButton>
+          <MyIconButton>
+            <MenuOutlinedIcon />
+          </MyIconButton>
+          <MyIconButton>
+            <NotificationsOutlinedIcon />
+          </MyIconButton>
+        </MyToolbar>
+      </AppBar>
+    </StyledEngineProvider>
+  );
+}
+
+const MyToolbar = styled(Toolbar)`
+  height: 10px;
+  background-color: #384b60;
+`;
+
+const MyIconButton = styled(IconButton)`
+  color: white;
+`;
 
 export default Header;
--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,3 +1,7 @@
-function MainPage() {}
+import Header from "../components/common/Header/Header";
+
+function MainPage() {
+  return <Header />;
+}
 
 export default MainPage;
